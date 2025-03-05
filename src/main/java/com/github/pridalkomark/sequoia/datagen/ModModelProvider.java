961515680a3af43ea171a8df1fa819424926a361
Pridalkó Márk <mark.pridalko@gmail.com>
2025-03-05 06:54:57 +0100

    additional wood type blocks and their boilerplate, version bump
    TODO: translation keys,
          creative inventory placement,
          crafting recipes
          the damn structure biome stuff

--- src/main/java/com/github/pridalkomark/sequoia/datagen/ModModelProvider.java
+++ src/main/java/com/github/pridalkomark/sequoia/datagen/ModModelProvider.java
@@ -13,10 +13,15 @@ public class ModModelProvider extends FabricModelProvider {
 
     @Override
     public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
-        blockStateModelGenerator.registerLog(ModBlocks.SEQUOIA_LOG).log(ModBlocks.SEQUOIA_LOG).wood(ModBlocks.SEQUOIA_WOOD);
-        blockStateModelGenerator.registerLog(ModBlocks.STRIPPED_SEQUOIA_LOG).log(ModBlocks.STRIPPED_SEQUOIA_LOG).wood(ModBlocks.STRIPPED_SEQUOIA_WOOD);
+        blockStateModelGenerator.registerLog(ModBlocks.SEQUOIA_LOG)
+                .log(ModBlocks.SEQUOIA_LOG).wood(ModBlocks.SEQUOIA_WOOD);
+        blockStateModelGenerator.registerLog(ModBlocks.STRIPPED_SEQUOIA_LOG)
+                .log(ModBlocks.STRIPPED_SEQUOIA_LOG).wood(ModBlocks.STRIPPED_SEQUOIA_WOOD);
         blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.SEQUOIA_PLANKS);
         blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.SEQUOIA_LEAVES);
+        blockStateModelGenerator.registerTrapdoor(ModBlocks.SEQUOIA_TRAPDOOR);
+        blockStateModelGenerator.registerDoor(ModBlocks.SEQUOIA_DOOR);
+        //blockStateModelGenerator.cre(ModBlocks.SEQUOIA_DOOR);                                                         https://youtu.be/TgDh216TelA?t=496
     }
 
     @Override
