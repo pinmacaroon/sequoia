c4f460e14e9465b4c959e665d4cb2e95fd8c4730
pinmacaroon <mark.pridalko@gmail.com>
2025-03-11 06:58:13 +0100

    added english translations, bumped version, added to item groups. door likes still have no textures, waiting for the texture department. preparing publish

--- src/main/java/com/github/pridalkomark/sequoia/item/ModItems.java
+++ src/main/java/com/github/pridalkomark/sequoia/item/ModItems.java
@@ -1,9 +1,11 @@
 package com.github.pridalkomark.sequoia.item;
 
 import com.github.pridalkomark.sequoia.Sequoia;
+import com.github.pridalkomark.sequoia.block.ModBlocks;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
@@ -14,6 +16,19 @@ public class ModItems {
     //public static final Item PINE_CONE = registerItem("pine_cone", new Item(new FabricItemSettings()));
 
     private static void addItemsToBuildingBlockItemGroup(FabricItemGroupEntries entries) {
+        entries.addAfter(Blocks.WARPED_BUTTON, ModBlocks.SEQUOIA_LOG);
+        entries.addAfter(ModBlocks.SEQUOIA_LOG, ModBlocks.SEQUOIA_WOOD);
+        entries.addAfter(ModBlocks.SEQUOIA_WOOD, ModBlocks.STRIPPED_SEQUOIA_LOG);
+        entries.addAfter(ModBlocks.STRIPPED_SEQUOIA_LOG, ModBlocks.STRIPPED_SEQUOIA_WOOD);
+        entries.addAfter(ModBlocks.STRIPPED_SEQUOIA_WOOD, ModBlocks.SEQUOIA_PLANKS);
+        entries.addAfter(ModBlocks.SEQUOIA_PLANKS, ModBlocks.SEQUOIA_STAIRS);
+        entries.addAfter(ModBlocks.SEQUOIA_STAIRS, ModBlocks.SEQUOIA_SLAB);
+        entries.addAfter(ModBlocks.SEQUOIA_SLAB, ModBlocks.SEQUOIA_FENCE);
+        entries.addAfter(ModBlocks.SEQUOIA_FENCE, ModBlocks.SEQUOIA_FENCE_GATE);
+        entries.addAfter(ModBlocks.SEQUOIA_FENCE_GATE, ModBlocks.SEQUOIA_DOOR);
+        entries.addAfter(ModBlocks.SEQUOIA_DOOR, ModBlocks.SEQUOIA_TRAPDOOR);
+        entries.addAfter(ModBlocks.SEQUOIA_TRAPDOOR, ModBlocks.SEQUOIA_BUTTON);
+        entries.addAfter(ModBlocks.SEQUOIA_BUTTON, ModBlocks.SEQUOIA_PRESSURE_PLATE);
     }
 
     private static void addItemsToIngredientsItemGroup(FabricItemGroupEntries entries) {
