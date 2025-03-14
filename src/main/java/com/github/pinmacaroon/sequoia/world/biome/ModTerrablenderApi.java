88894be8ad4f3c18135c5c6abee179034c8af581
pinmacaroon <mark.pridalko@gmail.com>
2025-03-14 20:22:29 +0100

    biome work yet again, but error on world gen: Caused by: java.lang.IllegalStateException: Feature order cycle found, involved sources: [Reference{ResourceKey[minecraft:worldgen/biome / minecraft:old_growth_pine_taiga]=net.minecraft.world.biome.Biome@d67714}, Reference{ResourceKey[minecraft:worldgen/biome / sequoia:sequoia_forest]=net.minecraft.world.biome.Biome@56903603}]

--- src/main/java/com/github/pinmacaroon/sequoia/world/biome/ModTerrablenderApi.java
+++ src/main/java/com/github/pinmacaroon/sequoia/world/biome/ModTerrablenderApi.java
@@ -1,13 +1,20 @@
 package com.github.pinmacaroon.sequoia.world.biome;
 
 import com.github.pinmacaroon.sequoia.Sequoia;
+import com.github.pinmacaroon.sequoia.world.biome.surface.ModMaterialRules;
 import net.minecraft.util.Identifier;
+import terrablender.api.RegionType;
 import terrablender.api.Regions;
+import terrablender.api.SurfaceRuleManager;
 import terrablender.api.TerraBlenderApi;
 
 public class ModTerrablenderApi implements TerraBlenderApi {
     @Override
     public void onTerraBlenderInitialized() {
-        Regions.register(new ModOverworldRegion(new Identifier(Sequoia.MOD_ID, "overworld"), ,4));         //TODO pls pin finish :3
+        Regions.register(new ModOverworldRegion(new Identifier(Sequoia.MOD_ID, "overworld"), RegionType.OVERWORLD,
+                4));
+
+        SurfaceRuleManager.addSurfaceRules(SurfaceRuleManager.RuleCategory.OVERWORLD, Sequoia.MOD_ID,
+                ModMaterialRules.makeRules());
     }
 }
--- src/main/java/com/github/pinmacaroon/sequoia/world/biome/surface/ModMaterialRules.java
+++ src/main/java/com/github/pinmacaroon/sequoia/world/biome/surface/ModMaterialRules.java
@@ -1,4 +1,31 @@
 package com.github.pinmacaroon.sequoia.world.biome.surface;
 
+import com.github.pinmacaroon.sequoia.world.biome.ModBiomes;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.world.gen.surfacebuilder.MaterialRules;
+
 public class ModMaterialRules {
+    private static final MaterialRules.MaterialRule DIRT = makeStateRule(Blocks.DIRT);
+    private static final MaterialRules.MaterialRule PODZOL = makeStateRule(Blocks.PODZOL);
+    private static final MaterialRules.MaterialRule GRASS_BLOCK = makeStateRule(Blocks.GRASS_BLOCK);
+
+    public static MaterialRules.MaterialRule makeRules() {
+        MaterialRules.MaterialCondition isAtOrAboveWaterLevel = MaterialRules.water(-1, 0);
+
+        MaterialRules.MaterialRule grassSurface = MaterialRules.sequence(MaterialRules.condition(isAtOrAboveWaterLevel,
+                GRASS_BLOCK), PODZOL);
+
+        return MaterialRules.sequence(
+                MaterialRules.sequence(MaterialRules.condition(MaterialRules.biome(ModBiomes.SEQUOIA_FOREST),
+                                MaterialRules.condition(MaterialRules.STONE_DEPTH_FLOOR, makeStateRule(Blocks.STONE))),
+                        MaterialRules.condition(MaterialRules.STONE_DEPTH_CEILING, makeStateRule(Blocks.STONE))),
+
+                MaterialRules.condition(MaterialRules.STONE_DEPTH_FLOOR, grassSurface)
+        );
+    }
+
+    private static MaterialRules.MaterialRule makeStateRule(Block block) {
+        return MaterialRules.block(block.getDefaultState());
+    }
 }
