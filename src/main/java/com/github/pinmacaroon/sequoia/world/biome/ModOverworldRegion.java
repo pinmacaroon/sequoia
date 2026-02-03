6687195b2cf9357f9e3ea1c99a0e53e666cf4ac7
pinmacaroon <pinmacaroon@users.noreply.github.com>
2026-02-03 17:21:51 +0100

    tried to fix the worldgen but its kinda hard without docs :P
    i kinda miss boxed tbh

--- src/main/java/com/github/pinmacaroon/sequoia/world/biome/ModOverworldRegion.java
+++ src/main/java/com/github/pinmacaroon/sequoia/world/biome/ModOverworldRegion.java
@@ -7,25 +7,34 @@ import net.minecraft.util.Identifier;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.BiomeKeys;
 import net.minecraft.world.biome.source.util.MultiNoiseUtil;
+import terrablender.api.ParameterUtils;
 import terrablender.api.Region;
 import terrablender.api.RegionType;
+import terrablender.api.VanillaParameterOverlayBuilder;
 
 import java.util.function.Consumer;
 
 public class ModOverworldRegion extends Region {
-    public ModOverworldRegion(Identifier name, RegionType type, int weight) {
-        super(name, type, weight);
+
+    public ModOverworldRegion(Identifier name, int weight)
+    {
+        super(name, RegionType.OVERWORLD, weight);
     }
 
     @Override
-    public void addBiomes(Registry<Biome> registry, Consumer<Pair<MultiNoiseUtil.NoiseHypercube,
-            RegistryKey<Biome>>> mapper) {
-        this.addModifiedVanillaOverworldBiomes(
-                mapper,
-                modifiedVanillaOverworldBuilder ->
-                        modifiedVanillaOverworldBuilder.replaceBiome(
-                                BiomeKeys.FOREST, ModBiomes.SEQUOIA_FOREST
-                        )
-        );
+    public void addBiomes(Registry<Biome> registry, Consumer<Pair<MultiNoiseUtil.NoiseHypercube, RegistryKey<Biome>>> mapper) {
+        VanillaParameterOverlayBuilder builder = new VanillaParameterOverlayBuilder();
+
+        new ParameterUtils.ParameterPointListBuilder()
+                .temperature(ParameterUtils.Temperature.span(ParameterUtils.Temperature.COOL, ParameterUtils.Temperature.FROZEN))
+                .humidity(ParameterUtils.Humidity.span(ParameterUtils.Humidity.ARID, ParameterUtils.Humidity.DRY))
+                .continentalness(ParameterUtils.Continentalness.INLAND)
+                .erosion(ParameterUtils.Erosion.EROSION_0, ParameterUtils.Erosion.EROSION_1)
+                .depth(ParameterUtils.Depth.SURFACE, ParameterUtils.Depth.FLOOR)
+                .weirdness(ParameterUtils.Weirdness.MID_SLICE_NORMAL_ASCENDING, ParameterUtils.Weirdness.MID_SLICE_NORMAL_DESCENDING)
+                .build().forEach(point -> builder.add(point, ModBiomes.SEQUOIA_FOREST));
+
+        builder.build().forEach(mapper);
+//        this.addModifiedVanillaOverworldBiomes(mapper, modifiedVanillaOverworldBuilder -> modifiedVanillaOverworldBuilder.replaceBiome(BiomeKeys.FOREST, ModBiomes.SEQUOIA_FOREST));
     }
 }
